4ecb58365f24459d154a316d1f84ca0ede6b272d
garnierk <Kevin_Garnier@outlook.fr>
2023-04-10 13:56:56 +0200

    add partial fix

--- lib/Matrix.ml
+++ lib/Matrix.ml
@@ -33,40 +33,45 @@ let mul_scalar mat k =
   let n, m = size mat in
   make (fun (i, j) -> k * get mat (i, j)) n m
 
-let assign_col mat cj k cj' k' =
+let assign_col mat cj k cj' =
   let assign (i, j) =
-    if j = cj then (mat.(i).(cj) * k) + (mat.(i).(cj') * k') else mat.(i).(j)
+    if j = cj then (mat.(i).(cj)) + (mat.(i).(cj') * k) else mat.(i).(j)
   in
   let n, m = size mat in
   make assign n m
 
-let change_sign mat t j =
-  assign_col mat j (-1) 0 0, assign_col t j (-1) 0 0
+let permut_and_assign mat cj k cj' = 
+  let assign (i,j) =
+    if j = cj then mat.(i).(cj)
+    else if j = cj' then mat.(i).(cj') - k * mat.(i).(cj)
+    else mat.(i).(j)
+  in 
+    let n,m = size mat in
+    make assign n m
 
-let reduce mat t li cj ck =
+let change_sign (mat,t) j =
+  let f change_sign_col m cj = 
+    let n,m = size m
+    make (fun (i,j) -> (if cj = j then (-m.(i).(j)) else m.(i).(j)))
+  assign_col mat j (-2) j, assign_col t j (-2) j
+
+let reduce (mat,t) li cj ck =
   let mat,t = if mat.(li).(cj) < 0 then change_sign mat t cj else mat,t in
   let mat,t = if mat.(li).(ck) < 0 then change_sign mat t ck else mat,t in
-  let u, v = Prime.bezout mat.(li).(cj) mat.(li).(ck) in
-  if u = 0 || v = 0 then let u = 1 and v = -1 in
-  assign_col mat cj u ck v, assign_col t cj u ck v
-  else assign_col mat cj u ck v, assign_col t cj u ck v
-
-let nullify mat t li cj ck =
-  let mat,t = if mat.(li).(cj) < 0 then change_sign mat t cj else mat,t in
-  let mat,t = if mat.(li).(ck) < 0 then change_sign mat t ck else mat,t in
-  let u = mat.(li).(ck) and v = mat.(li).(cj) in
-  assign_col mat cj (-u) ck v, assign_col t cj (-u) ck v
+  let k = mat.(li).(cj)/ mat.(li).(ck) in
+  assign_col mat cj (-k) ck, assign_col t cj (-k) ck  
 
 
 let rec hermite_loop (mat,t) i =
   let n,m = size mat in
   if i >= n || i >= m then (mat, t) else
-    let rec loop (mat,t) j =
-      Printf.printf "(%d,%d), dim = (%d,%d)\n" i j n m;
-      if j < i then loop (reduce mat t i j i) (j+1)
-      else if j = i then if mat.(i).(j) < 0 then loop (change_sign mat t i) (j+1) else loop (mat,t) (j+1)
-      else if j < m then loop (nullify mat t i j i) (j+1)
-      else mat,t
+    let rec loop mt j =
+      if j >= m then 
+        if is_reducted mt i then reduce_left mt i else loop (permut_min mt i) i
+      else if i == j then 
+        if mat.(i).(j) < 0 then loop (change_sign mt i) (j+1) 
+        else loop mt (j+1)
+      else loop (reduce mt i j i) (j+1)
   in hermite_loop (loop (mat,t) 0) (i+1)
 
 let hermite mat =
