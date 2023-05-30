4499b62795ce6b381edfa2ce67c2e28ac63f165f
napoknot21 <kinbrian5@hotmail.com>
2023-05-30 22:47:59 +0200

    add of script for runing the project

--- bin/main.ml
+++ bin/main.ml
@@ -1,4 +1,4 @@
-(* Include necessary modules *)
+(*(* Include necessary modules *)
 open Subgroups.Formula
 open Subgroups.Prime
 open Subgroups.Groups
@@ -59,3 +59,68 @@ let () =
       (fold_prime_couple (factorise n_value));
     Printf.printf "]\n";
   )
+*)
+(* Include necessary modules *)
+open Subgroups.Formula
+open Subgroups.Prime
+open Subgroups.Groups
+open Subgroups.Lattice
+
+(* Command line arguments *)
+let n = ref None
+let p = ref false
+let g = ref false
+let o = ref "output.dot"
+
+(* Specification of command line options *)
+let specs = [
+  ("-n", Arg.Int (fun x -> n := Some x), "Set the value of n");
+  ("-p", Arg.Set p, "Decomposition into prime numbers");
+  ("-g", Arg.Set g, "Whether to draw a graph (default is true)");
+  ("-o", Arg.String (fun s -> o := s), "Set the output file name")
+]
+
+let () =
+  Arg.parse specs print_endline "Usage: main.ml -n <n_value> [-p] [-g] [-o output_file_name]";
+
+  match !n with
+  | None -> 
+    Printf.printf "Please provide a valid value for n (n > 0).\n";
+    exit 1
+  | Some n_value ->
+    let prime_decomposition = !p in
+    let draw_graph = !g in
+    let output_file_name = !o in
+
+    if not draw_graph && output_file_name <> "output.dot" then (
+      Printf.printf "The -o flag can't be used without the -g flag.\n";
+      exit 1
+    );
+
+    Printf.printf "|Z/%dZ x Z/%dZ| = %d\n" n_value n_value (number_of_subgroups n_value);
+    Printf.printf "Generating subgroups...";
+    flush stdout;
+
+    let set = generate_subgroups 2 3 in
+    Printf.printf "done\nGenerating lattice...";
+    flush stdout;
+  
+    let lattice = make_lattice set in
+    Printf.printf "done\n";
+  
+    if draw_graph then (
+      Printf.printf "Writing the graph dot file...";
+      flush stdout;
+      let file = open_out output_file_name in
+      to_dot lattice file;
+      close_out file;
+      Printf.printf "done\n"
+    );
+
+    if prime_decomposition then (
+      Printf.printf "[";
+      List.iter
+        (fun (p, n) -> Printf.printf "(%d,%d);" p n)
+        (fold_prime_couple (factorise n_value));
+      Printf.printf "]\n";
+    )
--- run.sh
+++ run.sh
@@ -1,7 +1,51 @@
 #!/bin/bash
 
-#exect the project
-ocamlopt -o main -I ./lib/ ./bin/main.ml
+# Define a usage function
+usage() {
+  echo "Usage: $0 -n <n_value> [-p] [-g] [-o output_file_name]"
+  exit 1
+}
+
+# Initialize parameters
+n_value=""
+p_flag=""
+g_flag=""
+o_value=""
+
+# Parse command-line arguments
+while getopts ":n:pgo:" opt; do
+  case ${opt} in
+    n )
+      n_value=$OPTARG
+      ;;
+    p )
+      p_flag="-p"
+      ;;
+    g )
+      g_flag="-g"
+      ;;
+    o )
+      o_value="-o $OPTARG"
+      ;;
+    \? )
+      usage
+      ;;
+    : )
+      echo "Invalid option: $OPTARG requires an argument" 1>&2
+      usage
+      ;;
+  esac
+done
+shift $((OPTIND -1))
+
+# Check that n was provided
+if [ -z "$n_value" ]; then
+  echo "You must provide a value for n."
+  usage
+fi
+
+# Run the OCaml program with the specified parameters
+dune exec bin/main.exe -- -n "$n_value" $p_flag $g_flag $o_value
 
 # Run the project
 # dune exec subgroups
